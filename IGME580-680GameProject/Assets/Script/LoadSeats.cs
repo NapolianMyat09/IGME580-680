cf619773523e568934f96e7721cd9c3de8a5752b
nm8011 <nm8011@rit.edu>
2025-02-26 10:15:21 -0500

    seat asset added

---
+++ IGME580-680GameProject/Assets/Script/LoadSeats.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSeats : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for (int i = 0; i < numOfSeats; i++)
+        //{
+        //    float angle = i * Mathf.PI * 2 / numOfSeats;
+        //    float x = Mathf.Cos(angle) * radius;
+        //    float z = Mathf.Sin(angle) * radius;
+        //    Vector3 position = new Vector3(x, 0, z);
+        //    GameObject prop = Instantiate(propPrefab, position, Quaternion.identity, transform);
+        //    prop.transform.LookAt(transform.position);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
