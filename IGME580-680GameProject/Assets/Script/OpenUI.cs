e98610dcaa2f0b143fbe8592cd7cf4e84d86fa07
nm8011 <nm8011@rit.edu>
2025-03-19 20:18:51 -0400

    button working, no interactivity yet

--- IGME580-680GameProject/Assets/Script/OpenUI.cs
+++ IGME580-680GameProject/Assets/Script/OpenUI.cs
@@ -6,13 +6,13 @@ public class OpenUI : MonoBehaviour
 {
     [SerializeField]
     GameObject triggerObj; //if camera gets close to this object, will trigger UI options
+    [SerializeField]
+    FirstPersonLook toggleCameraMovement; //will require so players dont move their camera around and can actually click the button to open UI
+     
     // Start is called before the first frame update
-    
-
     void Start()
     {
         triggerObj.SetActive(false);
-        Debug.Log("Set Inactive");
     }
 
     // Update is called once per frame
@@ -20,19 +20,34 @@ public class OpenUI : MonoBehaviour
     {
         
     }
+
+    /// <summary>
+    /// When in close promixity of parent object button is assigned, shows UI button
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) // Ensure only the player triggers it
+        if (other.CompareTag("Player"))
         {
-            triggerObj.SetActive(true); // Show the UI
+            triggerObj.SetActive(true);
+            toggleCameraMovement.enabled = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
     }
 
+    /// <summary>
+    /// When far from proximity of parent object button is assigned, hides UI button
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            triggerObj.SetActive(false); // Hide the UI
+            triggerObj.SetActive(false);
+            toggleCameraMovement.enabled = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
     }
 
